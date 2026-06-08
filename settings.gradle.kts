0709ce05cff5715d6c76a1dc97242c8d93752c84
Kishku7 <Kishku7@users.noreply.github.com>
2026-06-08 09:02:20 -0600

    Reorganize platform modules into plugin/ and mods/ groups
    - plugin/spigot (was bukkit; module renamed :chunky-spigot), plugin/paperMC (paper),
      plugin/folia (folia)
    - mods/fabric, mods/neoforge
    - nbt/ and common/ remain at root as shared core
    - settings.gradle.kts maps the new project dirs; CI build.yml updated for the new
      paths, Java 25, and the dropped Forge/Sponge artifacts.
    
    Verified: :chunky-spigot, :chunky-fabric, :chunky-neoforge all shadowJar clean.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,15 +10,23 @@ pluginManagement {
 
 rootProject.name = "chunky"
 
-sequenceOf(
-    "nbt",
-    "common",
-    "paper",
-    "folia",
-    "bukkit",
-    "fabric",
-    "neoforge"
-).forEach {
-    include("${rootProject.name}-$it")
-    project(":${rootProject.name}-$it").projectDir = file(it)
-}
+include("chunky-nbt")
+project(":chunky-nbt").projectDir = file("nbt")
+
+include("chunky-common")
+project(":chunky-common").projectDir = file("common")
+
+include("chunky-spigot")
+project(":chunky-spigot").projectDir = file("plugin/spigot")
+
+include("chunky-paper")
+project(":chunky-paper").projectDir = file("plugin/paperMC")
+
+include("chunky-folia")
+project(":chunky-folia").projectDir = file("plugin/folia")
+
+include("chunky-fabric")
+project(":chunky-fabric").projectDir = file("mods/fabric")
+
+include("chunky-neoforge")
+project(":chunky-neoforge").projectDir = file("mods/neoforge")
