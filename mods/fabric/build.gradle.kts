a5818210af06d1f326ddcd8456499fe28f416316
Kishku7 <Kishku7@users.noreply.github.com>
2026-06-11 15:47:38 -0600

    1.0.2: update Fabric to Minecraft 26.2-rc-1 (verified across the 26.2 line)

--- mods/fabric/build.gradle.kts
+++ mods/fabric/build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
 val shade: Configuration by configurations.creating
 
 dependencies {
-    minecraft(group = "com.mojang", name = "minecraft", version = "26.1")
-    implementation(group = "net.fabricmc", name = "fabric-loader", version = "0.18.5")
-    implementation(group = "net.fabricmc.fabric-api", name = "fabric-api", version = "0.144.3+26.1")
+    minecraft(group = "com.mojang", name = "minecraft", version = "26.2-rc-1")
+    implementation(group = "net.fabricmc", name = "fabric-loader", version = "0.19.3")
+    implementation(group = "net.fabricmc.fabric-api", name = "fabric-api", version = "0.152.0+26.2")
     compileOnly(group = "me.lucko", name = "fabric-permissions-api", version = "0.7.0")
     implementation(project(":chunky-common"))
     shade(project(":chunky-common"))
