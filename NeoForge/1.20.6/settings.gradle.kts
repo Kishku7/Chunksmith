c59819d99ce1c09076b7961519cf82768fce46b0
Kishku7 <Kishku7@users.noreply.github.com>
2026-07-01 01:02:35 -0600

    Goal5: graft NeoForge/1.20.6 (transitional, builds green)

---
+++ NeoForge/1.20.6/settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.architectury.dev/")
+        maven("https://maven.neoforged.net/releases/")
+        maven("https://maven.minecraftforge.net/")
+        maven("https://repo.spongepowered.org/repository/maven-public/")
+        maven("https://repo.codemc.io/repository/relativitymc/")
+    }
+}
+
+rootProject.name = "chunksmith-neoforge-1.20.6"
+
+include("chunksmith-common")
+project(":chunksmith-common").projectDir = file("../../shared_common")
